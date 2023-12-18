e12a0374685f4485724a2014dd53be6b5a7fdd3a
mythpoy <okmlnsunok@gmail.com>
2023-12-18 22:33:28 +0900

    feat : P1874

---
+++ datastructure/P1874.java
@@ -0,0 +1,49 @@
+package org.example.datastructure;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class P1874 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        StringBuffer bf = new StringBuffer();
+
+        int num = 1; // 오름차순 수
+        boolean result = true;
+
+        for (int i = 0; i < A.length; i++) {
+            int su = A[i];
+            if (su >= num) {
+                while (su >= num) {
+                    stack.push(num++);
+                    bf.append("+\n");
+                }
+                stack.pop();
+                bf.append("-\n");
+            } else { // 현재 수열 값 < 오름차순 자연수 일 경우 pop() 수행
+                int n = stack.pop();
+
+                //만약 스택의 최상단 수가 수열의 수보다 크면
+                if (n > su) {
+                    System.out.println("NO");
+                    result = false;
+                    break;
+                }
+                else {
+                    bf.append("-\n");
+                }
+            }
+        }
+        if (result) {
+            System.out.println(bf.toString());
+        }
+    }
+}
