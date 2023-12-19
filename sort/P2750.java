72d3341d77fb59db02854661e65b5f703ad24fe5
mythpoy <okmlnsunok@gmail.com>
2023-12-19 21:51:38 +0900

    feat : P2750

---
+++ sort/P2750.java
@@ -0,0 +1,29 @@
+package org.example.sort;
+
+import java.util.Scanner;
+
+public class P2750 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr  = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N-1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
