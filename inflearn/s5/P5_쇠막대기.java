ddc865f3beea6465777a985a94f4ad95ebe94f52
mythpoy <okmlnsunok@gmail.com>
2024-02-27 17:44:40 +0900

    feat(5) : P5

---
+++ inflearn/s5/P5_쇠막대기.java
@@ -0,0 +1,26 @@
+package org.example.inflearn.s5;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class P5_쇠막대기 {
+    public static int solution(String str) {
+        int answer=0;
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<str.length(); i++) {
+            if(str.charAt(i)=='(') stack.push('(');
+            else {
+                stack.pop();
+                if(str.charAt(i-1)=='(') answer+=stack.size();
+                else answer+=1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(solution(str));
+    }
+}
