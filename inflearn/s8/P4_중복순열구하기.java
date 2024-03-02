2a27a36fc2406ff3161d5f6eb3ed0c5c13ae1259
mythpoy <okmlnsunok@gmail.com>
2024-03-02 22:42:27 +0900

    feat(8) : P4

---
+++ inflearn/s8/P4_중복순열구하기.java
@@ -0,0 +1,27 @@
+package org.example.inflearn.s8;
+
+import java.util.Scanner;
+
+public class P4_중복순열구하기 {
+    static int n, m;
+    static int[] answer;
+    public static void DFS(int L) {
+        if(L==m) {
+            for(int i=0; i<m; i++) System.out.print(answer[i] + " ");
+            System.out.println();
+        }
+        else {
+            for(int i=1; i<=n; i++) {
+                answer[L]=i;
+                DFS(L+1);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        answer = new int[m];
+        DFS(0);
+    }
+}
