8df23d3521000893fb6e97951ecd83d94d8fb762
mythpoy <okmlnsunok@gmail.com>
2024-03-03 22:21:58 +0900

    feat(8) : P5~P6

---
+++ inflearn/s8/P5_동전교환.java
@@ -0,0 +1,35 @@
+package org.example.inflearn.s8;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P5_동전교환 {
+    static int n, m;
+    static int[] coins;
+    static int answer=Integer.MAX_VALUE;
+    public static void DFS(int L, int sum) {
+        if(sum>m) return;
+        if(L>=answer) return;
+        if(sum==m) {
+//            System.out.println(sum + " " + L);
+            answer=Math.min(answer, L);
+        }
+        else {
+            for(int i=0; i<n; i++){
+                DFS(L+1, sum+coins[n-1-i]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt();
+        coins=new int[n];
+        for(int i=0; i<n; i++){
+            coins[i]=sc.nextInt();
+        }
+        Arrays.sort(coins);
+        m=sc.nextInt();
+        DFS(0, 0);
+        System.out.println(answer);
+    }
+}
---
+++ inflearn/s8/P6_순열.java
@@ -0,0 +1,28 @@
+package org.example.inflearn.s8;
+
+import java.util.Scanner;
+
+public class P6_순열 {
+    static int n, m;
+    static int[] arr;
+    public static void DFS(int L, int prev, String answer) {
+        if(L==m){
+            System.out.println(answer);
+        }else{
+            for(int i=0; i<n; i++){
+                if(arr[i]!=prev) DFS(L+1, arr[i], answer+arr[i]+" ");
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt();
+        m=sc.nextInt();
+        arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        DFS(0,0,"");
+    }
+}
