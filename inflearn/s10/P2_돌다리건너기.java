e090c2b2dcd7532454934458c94b946d77ba2bc7
mythpoy <okmlnsunok@gmail.com>
2024-03-09 18:26:40 +0900

    feat(10) : P1~P4

---
+++ inflearn/s10/P2_돌다리건너기.java
@@ -0,0 +1,21 @@
+package org.example.inflearn.s10;
+
+import java.util.Scanner;
+
+public class P2_돌다리건너기 {
+    static int[] dp;
+    public static int solution(int n){
+        dp = new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3; i<=n; i++){
+            dp[i]=dp[i-2]+dp[i-1];
+        }
+        return dp[n-1]+dp[n];
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(solution(n));
+    }
+}
