589e2026c0c9c34310ce2430843eaf7b4442841f
mythpoy <okmlnsunok@gmail.com>
2024-03-09 16:03:56 +0900

    feat(9) : P4~P6

---
+++ inflearn/Template.java
@@ -0,0 +1,14 @@
+package org.example.inflearn;
+
+import java.util.*;
+public class Template {
+    public static int solution(){
+        int answer=0;
+
+        return answer;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
