79b616d83f4d466bbf405a6c331fda64dce0eb2a
mythpoy <okmlnsunok@gmail.com>
2024-03-15 23:56:16 +0900

    feat(2) : P3

--- inflearn/normal/s3/P3.java
+++ inflearn/normal/s3/P3.java
@@ -1,8 +1,45 @@
 package org.example.inflearn.normal.s3;
 import java.util.*;
+
 public class P3 {
     public int[] solution(int[] arrival, int[] state){
-        int[] answer = {};
+        Queue<Integer> enter = new LinkedList<>();
+        Queue<Integer> exit = new LinkedList<>();
+        int n = arrival.length;
+        int[] answer = new int[n];
+        int prev = 1;
+        for(int t = 0, i = 0, cnt = 0; ; t++){
+            if(enter.isEmpty() && exit.isEmpty() && i<n){
+                if(t < arrival[i]){ // 시간 건너뛰기
+                    t = arrival[i];
+                    prev = 1;
+                }
+            }
+            while(i < n && arrival[i] <= t){
+                if(state[i]==0) enter.offer(i);
+                else exit.offer(i);
+                i++;
+            }
+            if(prev==1){
+                if(!exit.isEmpty()){
+                    answer[exit.poll()] = t;
+                    prev=1;
+                } else {
+                    answer[enter.poll()] = t;
+                    prev=0;
+                }
+            } else if(prev==0){
+                if(!enter.isEmpty()){
+                    answer[enter.poll()] = t;
+                    prev=0;
+                } else {
+                    answer[exit.poll()] = t;
+                    prev=1;
+                }
+            }
+            cnt++;
+            if(cnt == n) break; // 현관문 사용한 숫자와 사람 수가 같아질 때 중단
+        }
 
         return answer;
     }
@@ -10,7 +47,7 @@ public class P3 {
     public static void main(String[] args){
         P3 T = new P3();
         System.out.println(Arrays.toString(T.solution(new int[]{0, 1, 1, 1, 2, 3, 8, 8}, new int[]{1, 0, 0, 1, 0, 0, 1, 0})));
-        System.out.println(Arrays.toString(T.solution(new int[]{3, 3, 4, 5, 5, 5}, new int[]{1, 0, 1, 0, 1, 0})));
-        System.out.println(Arrays.toString(T.solution(new int[]{2, 2, 2, 3, 4, 8, 8, 9, 10, 10}, new int[]{1, 0, 0, 0, 1, 1, 0, 1, 1, 0})));
+//        System.out.println(Arrays.toString(T.solution(new int[]{3, 3, 4, 5, 5, 5}, new int[]{1, 0, 1, 0, 1, 0})));
+//        System.out.println(Arrays.toString(T.solution(new int[]{2, 2, 2, 3, 4, 8, 8, 9, 10, 10}, new int[]{1, 0, 0, 0, 1, 1, 0, 1, 1, 0})));
     }
 }
