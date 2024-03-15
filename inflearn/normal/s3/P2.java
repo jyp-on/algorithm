34d4006f595d8de0b51d845ea7ff8340e79cdffd
mythpoy <okmlnsunok@gmail.com>
2024-03-15 21:52:21 +0900

    feat(2) : P2(해설추가)

--- inflearn/normal/s3/P2.java
+++ inflearn/normal/s3/P2.java
@@ -42,12 +42,43 @@ public class P2 {
         return answer;
     }
 
+    public String solution2(String s){
+        String answer = "";
+        Stack<String> stack = new Stack<>();
+        for(Character x : s.toCharArray()){
+            if(x == ')'){
+                String tmp = "";
+                while(!stack.empty()){
+                    String c = stack.pop();
+                    if(c.equals("(")){
+                        String num = "";
+                        while(!stack.empty() && Character.isDigit(stack.peek().charAt(0))){
+                            num = stack.pop() + num;
+                        }
+                        String res = "";
+                        int cnt = 0;
+                        if(num.equals("")) cnt = 1;
+                        else cnt = Integer.parseInt(num);
+                        for(int i=0; i<cnt; i++) res += tmp;
+                        stack.push(res);
+                        break;
+                    }
+                    tmp = c+tmp;
+                }
+            } else {
+                stack.push(String.valueOf(x));
+            }
+        }
+        for(String x : stack) answer += x;
+        return answer;
+    }
+
     public static void main(String[] args){
         P2 T = new P2();
-//        System.out.println(T.solution("3(a2(b))ef"));
-//        System.out.println(T.solution("2(ab)k3(bc)"));
-//        System.out.println(T.solution("2(ab3((cd)))"));
-//        System.out.println(T.solution("2(2(ab)3(2(ac)))"));
-//        System.out.println(T.solution("3(ab2(sg))"));
+        System.out.println(T.solution2("3(a20(x))ef"));
+        System.out.println(T.solution2("2(ab)k3(bc)"));
+        System.out.println(T.solution2("2(ab3((cd)))"));
+        System.out.println(T.solution2("2(2(ab)3(2(ac)))"));
+        System.out.println(T.solution2("3(ab2(sg))"));
     }
 }
