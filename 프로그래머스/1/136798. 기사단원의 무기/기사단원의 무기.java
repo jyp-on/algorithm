d38c6601caa1a0010ce132bdd83556e3e92cd51e
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-04-19 22:20:23 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.47 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -8,9 +8,9 @@ class Solution {
     }
     private int getCnt(int n, int limit, int power) {
         int cnt = 0;
-        for(int i = 1; i <= Math.sqrt(n); i++) {
-            if(i==Math.sqrt(n) && n%i==0) cnt+=1;
-            else if(n%i==0) cnt+=2;
+        for(int i = 1; i <= Math.sqrt(n); i++) { // 제곱근 까지만 확인
+            if(i==Math.sqrt(n) && n%i==0) cnt+=1; // i*i==n 에서 i가 제곱근이라면 약수는 하나증가
+            else if(n%i==0) cnt+=2; // 그 외에는 약수가 대칭 되어있으므로 2개씩 증가
             
             if(cnt>limit) return power;
         }
