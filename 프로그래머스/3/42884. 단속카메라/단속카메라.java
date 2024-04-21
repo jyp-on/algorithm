d1aea182637250bcac9f2489c442191bc067ef3e
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-04-21 16:47:41 +0900

    [level 3] Title: 단속카메라, Time: 6.66 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,38 +1,15 @@
 import java.util.*;
 class Solution {
-    class Route implements Comparable<Route> {
-        int s, e;
-        public Route(int s, int e) {
-            this.s = s;
-            this.e = e;
-        }
-        @Override
-        public int compareTo(Route ob) {
-            return this.e - ob.e;
-        }
-    }
     public int solution(int[][] routes) {
-        int answer = 1;
-        List<Route> routeList = new ArrayList<>();
-        
-        for(int[] r : routes) {
-            routeList.add(new Route(r[0], r[1]));
-        }
-        
-        Collections.sort(routeList);
-        
-        // for(Route r : routeList) {
-        //     System.out.println(r.s + " " + r.e);
-        // }
-        
-        int last = routeList.get(0).e;
-        for(int i=1; i<routeList.size(); i++) {
-            Route next = routeList.get(i);
-            if(next.s > last) {
-                last = next.e;
-                answer++;
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
+        int ans = 0;
+        int last_camera = Integer.MIN_VALUE;
+        for (int[] a : routes) {
+            if (last_camera < a[0]) {
+                ++ans;
+                last_camera = a[1];
             }
         }
-        return answer;
+        return ans;
     }
 }
