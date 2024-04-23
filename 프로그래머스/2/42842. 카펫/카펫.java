b4cb27fbb7997dbbcb02aa3284387e5bb769517e
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-04-23 17:39:05 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        int sum = brown + yellow;
+        for(int col=3; col<sum; col++) {
+            int row = sum / col;
+            if(row < 3) continue;
+            if((row-2) * (col-2) == yellow) {
+                return new int[]{row, col};
+            }
+        }
+        return answer;
+    }
+}
