d3d8874342d4a621f2a43b85d91a8eaca773352c
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-05-01 19:55:47 +0900

    [Silver III] Title: N과 M (3), Time: 580 ms, Memory: 312580 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    static void DFS(int depth) {
+        if(depth == M) {
+            for(int i = 0; i < M; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+        } else {
+            for(int i = 1; i <= N; i++) {
+                arr[depth] = i;
+                DFS(depth + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+//        visited = new boolean[N + 1];
+        arr = new int[M];
+        DFS(0);
+        System.out.println(sb.toString());
+
+//        bw.write(answer.toString() + "\n");
+//        bw.flush();
+//        bw.close();
+//        br.close();
+    }
+
+}
