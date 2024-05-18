2bcbcf8ba2f1dc63f6783bf1d6fa1414c5886454
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-05-18 17:30:11 +0900

    [Silver III] Title: N과 M (5), Time: 332 ms, Memory: 68924 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -4,8 +4,9 @@ import java.util.*;
 public class Main {
 
     static int n, m;
-    static int[] arr;
+    static int[] arr, ans;
     static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -13,25 +14,30 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
         arr = new int[n];
+        ans = new int[n];
         visit = new boolean[n];
         for(int i=0; i<n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-        select(0, "");
+        select(0);
+        System.out.println(sb);
     }
 
-    static void select(int L, String str) {
+    static void select(int L) {
         if(L==m) {
-            str = str.replaceFirst(" ", "");
-            System.out.println(str);
+            for(int i=0; i<m; i++) {
+                sb.append(ans[i]+ " ");
+            }
+            sb.append("\n");
             return;
         }
 
         for(int i=0; i<n; i++) {
             if(!visit[i]) {
                 visit[i] = true;
-                select(L+1, str + " " + arr[i]);
+                ans[L] = arr[i];
+                select(L+1);
                 visit[i] = false;
             }
         }
