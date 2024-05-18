de2e43fe7bb26fe030a51bf96c71951776e6684b
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-05-18 17:51:38 +0900

    [Silver III] Title: N과 M (6), Time: 84 ms, Memory: 11560 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr, ans;
+    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        arr = new int[n];
+        ans = new int[m];
+        visit = new boolean[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        select(0, 0);
+        System.out.println(sb);
+    }
+
+    static void select(int depth, int start) {
+        if(depth==m) {
+            for(int i=0; i<m; i++) {
+                sb.append(ans[i]+ " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=start; i<n; i++) {
+            if(!visit[i]) {
+                visit[i] = true;
+                ans[depth] = arr[i];
+                select(depth+1, i);
+                visit[i] = false;
+            }
+
+        }
+    }
+}
