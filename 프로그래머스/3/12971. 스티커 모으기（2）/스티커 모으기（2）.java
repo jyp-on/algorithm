dfe68b2034ac90b40f151d883427aada807608a8
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-05-28 19:12:26 +0900

    [level 3] Title: 스티커 모으기(2), Time: 7.17 ms, Memory: 58.5 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -7,21 +7,24 @@ class Solution {
 
         int [] dp = new int[len];
 
-        // 첫 번째 스티커를 뗐을때
+        // 첫 번째 스티커를 뗐을때 (마지막 스티커는 뗄 수 없음)
         dp[0] = sticker[0];
         dp[1] = dp[0];
-        for (int i = 2; i < len - 1; i++) {
-            dp[i] = Math.max(dp[i - 2] + sticker[i], dp[i - 1]);
+        for(int i=2; i<len-1; i++) {
+            dp[i] = Math.max(dp[i-2] + sticker[i], dp[i-1]);
         }
-        answer = dp[len - 2];
 
-        // 첫 번째 스티커를 안뗐을때
+        answer = dp[len-2];
+        
+        // 첫 번째 스티커를 안뗏을 때 (마지막 스티커 뗄 수 있음)
         dp[0] = 0;
         dp[1] = sticker[1];
-        for (int i = 2; i < len; i++) {
-            dp[i] = Math.max(dp[i - 2] + sticker[i], dp[i - 1]);
+        for(int i=2; i<len; i++) {
+            dp[i] = Math.max(dp[i-2] + sticker[i], dp[i-1]);
         }
-        answer = Math.max(answer, dp[len - 1]);
+        
+        answer = Math.max(answer, dp[len-1]);
+        
         return answer;
     }
 }
