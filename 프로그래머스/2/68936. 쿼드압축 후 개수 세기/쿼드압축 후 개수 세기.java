f7af409e2092f56b8b763b2e8946ed16c25ced90
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-05-28 23:40:47 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 3.26 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -14,10 +14,10 @@ class Solution {
             else answer[0]++; // 압축이 0으로 됬다면
             return ;
         }
-        quad(arr,x,y, size/2);
-        quad(arr,x,y + size/2, size/2);
-        quad(arr,x+size/2,y, size/2);
-        quad(arr,x+size/2,y + size/2, size/2);
+        quad(arr,x,y, size/2); // 왼쪽 위
+        quad(arr,x,y + size/2, size/2); // 왼쪽 아래
+        quad(arr,x+size/2,y, size/2); // 오른쪽 위
+        quad(arr,x+size/2,y + size/2, size/2); // 오른쪽 아래
     }
     public int[] solution(int[][] arr) {
         answer = new int[2];
