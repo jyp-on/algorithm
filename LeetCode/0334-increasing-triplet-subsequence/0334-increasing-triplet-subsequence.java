3d0287e8749a9d26966b6ecc34b775649be4215d
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-07-15 08:58:25 +0900

    Time: 1 ms (100%), Space: 134.3 MB (8.04%) - LeetHub

---
+++ LeetCode/0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        if(nums.length < 3) {
+            return false;
+        }
+
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+
+        for(int n : nums) {
+            if(n <= a) {
+                a = n; 
+            } else if(n <= b) {
+                b = n;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
