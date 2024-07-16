831dc9140e5d225bd03c3f69b395d5b003e69aba
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-07-16 21:26:57 +0900

    [Silver III] Title: 모든 순열, Time: 192 ms, Memory: 24244 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] arr, ans;
+    static boolean[] visit;
+    static int N;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        arr = new int[N];
+        ans = new int[N];
+        visit = new boolean[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = i+1;
+        }
+        dfs(0);
+        System.out.println(sb.toString());
+    }
+
+    static void dfs(int depth) {
+        if(depth == N) {
+            for(int i=0; i<N; i++) {
+                sb.append(ans[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0; i<N; i++) {
+            if(!visit[i]) {
+                visit[i] = true;
+                ans[depth] = arr[i];
+                dfs(depth+1);
+                visit[i] = false;
+            }
+
+        }
+
+    }
+}
