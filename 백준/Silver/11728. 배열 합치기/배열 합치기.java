99fe3be5b9e4c4255f42e79188730b5d59e7b44b
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-07-16 10:55:15 +0900

    [Silver V] Title: 배열 합치기, Time: 1076 ms, Memory: 331708 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int size1 = Integer.parseInt(st.nextToken());
+        int size2 = Integer.parseInt(st.nextToken());
+        int[] arr1 = new int[size1];
+        int[] arr2 = new int[size2];
+        int[] ans = new int[size1 + size2];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < size1; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < size2; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int p1 = 0, p2 = 0, p3 = 0;
+        while (p1 < size1 && p2 < size2) {
+            if (arr1[p1] <= arr2[p2]) {
+                ans[p3++] = arr1[p1++];
+            } else {
+                ans[p3++] = arr2[p2++];
+            }
+        }
+
+        // Copy remaining elements of arr1, if any
+        while (p1 < size1) {
+            ans[p3++] = arr1[p1++];
+        }
+
+        // Copy remaining elements of arr2, if any
+        while (p2 < size2) {
+            ans[p3++] = arr2[p2++];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int x : ans) {
+            sb.append(x).append(" ");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
