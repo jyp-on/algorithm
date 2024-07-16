1b5bbf14c37a5e275d3e7a047f33f15d9cd194e3
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-07-16 22:20:35 +0900

    [Gold V] Title: 강의실 배정, Time: 680 ms, Memory: 79972 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,41 +1,38 @@
-
-
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static class Room implements Comparable<Room> {
-        int s, e;
-        public Room(int s, int e) {
-            this.s = s;
-            this.e = e;
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
 
-        @Override
-        public int compareTo(Room o) {
-            if(this.s == o.s) return this.e - o.e;
-            return this.s - o.s;
-        }
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        List<Room> rooms = new ArrayList<>();
-        for(int i=0; i<N; i++) {
-            rooms.add(new Room(sc.nextInt(), sc.nextInt()));
-        }
-        Collections.sort(rooms);
-        // 사용중인 회의실 pq를 끝나는시간 오름차순
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.offer(rooms.get(0).e);
-
-        for(int i=1; i<rooms.size(); i++) {
-            // 가장 빨리 끝나는 시각과 다음 강의 시작시간을 비교
-            if(pq.peek() <= rooms.get(i).s) {
-                pq.poll(); // 이미 회의가 끝난 강의실이니 pop
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+            // 시작시간 오름차순 정렬, 시작시간 같다면 종료시간 오름차순 정렬
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            } else {
+                return o1[0] - o2[0];
             }
-            pq.offer(rooms.get(i).e); // 회의를 진행해야 하니 offer
+        });
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            pq.add(new int[]{start, end});
         }
 
-        System.out.println(pq.size()); // 사용중인 회의실 개수
+        PriorityQueue<Integer> room = new PriorityQueue<>();
+        room.add(pq.poll()[1]); // 끝나는 시간
+
+        for (int i = 1; i < N; i++) {
+            if(room.peek() <= pq.peek()[0]) {
+                room.poll();
+            }
+            room.add(pq.poll()[1]);
+        }
+
+        System.out.println(room.size());
     }
 }
