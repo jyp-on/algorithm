bff17afd41a7dcb0e15756f5d4bcbae2429176bd
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-07-18 08:55:09 +0900

    [level 2] Title: 리코쳇 로봇, Time: 4.07 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -9,6 +9,7 @@ class Solution {
         int[] start = new int[2];
         int[] end = new int[2];
         
+        // 시작지점, 목표지점 위치 뽑기
         for(int i=0; i<row_size; i++) {
             for(int j=0; j<col_size; j++) {
                 if(board[i].charAt(j) == 'R') {
@@ -32,9 +33,12 @@ class Solution {
         int cnt = 0;
         
         while(!q.isEmpty()) {
-            int size = q.size();
+            // 한번에 큐에있는걸 모두 사용해서 동시에 뻗어나가게 하기
+            // 큐에 있는걸 한번 다 쓰면 cnt가 하나 증가하도록
+            int size = q.size(); 
             for(int i=0; i<size; i++) {
                 int[] cur = q.poll();
+                
                 // 방향
                 for(int j=0; j<4; j++) {
                     // 전역으로 변수 잡기위해 초반에 ny, nx를 만들어 둠
@@ -46,10 +50,13 @@ class Solution {
                         if(ny < 0 || ny >= row_size || nx < 0 || nx >= col_size
                            || board[ny].charAt(nx) == 'D') {
                             
+                            // 맵 밖으로 나가거나 장애물 만나면 직전 1칸으로 회귀
                             ny -= dy[j];
                             nx -= dx[j];
                             break;
                         } else {
+                            
+                            // 정상적으로 갈 수 있으면 같은 방향으로 한칸 이동
                             ny += dy[j];
                             nx += dx[j];
                         }
@@ -60,12 +67,14 @@ class Solution {
                         return cnt+1;
                     }
                     
+                    // 방문하지 않은 곳인 경우 방문처리 후 큐에 넣기
                     if(!visited[ny][nx]) {
                         visited[ny][nx] = true;
                         q.offer(new int[]{ny, nx});
                     }
                 }
             }
+            
             cnt ++;
         }
         
