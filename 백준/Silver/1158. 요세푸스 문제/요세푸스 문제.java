1816cad519799daa88a50c7192b0f64c703ffdde
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-07-22 18:40:04 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,40 +1,36 @@
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    static int N, K;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        Queue<Integer> q = new LinkedList<>();
-        int[] answer = new int[N];
-        for(int i=0; i<N; i++) {
-            q.add(i+1);
+        // 입력 처리
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+
+        // 사람들을 리스트에 초기화
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            list.add(i);
         }
-
-        int idx = 0;
-        int cnt = 0;
-        while(!q.isEmpty()) {
-            cnt++;
-            int x = q.poll();
-            if(cnt==K) {
-                answer[idx++] = x;
-                cnt = 0;
-            } else {
-                q.add(x);
-            }
-        }
-
 
         StringBuilder sb = new StringBuilder();
         sb.append("<");
-        for(int i=0; i<N; i++) {
-            sb.append(answer[i] + ", ");
+        int index = 0; // 현재 인덱스
+
+        while (list.size() > 0) {
+            // M번째 사람의 인덱스를 계산
+            index = (index + M - 1) % list.size();
+
+            // M번째 사람을 제거하고 결과에 추가
+            sb.append(list.remove(index) + ", ");
         }
-        sb.delete(sb.length()-2, sb.length());
+
+        // 결과 출력
+
+        sb.delete(sb.length() - 2, sb.length());
         sb.append(">");
         System.out.println(sb.toString());
     }
