80c5c8adec3936427e5581df93b63e56c1543a2e
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-08-07 19:01:50 +0900

    [Bronze III] Title: 이진수, Time: 96 ms, Memory: 14020 KB -BaekjoonHub

--- 백준/Bronze/3460. 이진수/이진수.java
+++ 백준/Bronze/3460. 이진수/이진수.java
@@ -1,31 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
-public class Main {
+class Main {
     static StringBuilder sb = new StringBuilder();
-    static StringBuilder answer = new StringBuilder();
-    public static void recursive(int n) {
-        if (n == 0) return;
-        int mod = n % 2;
-        sb.append(mod);
-        recursive(n / 2);
+    
+    static void binary(int num, int idx) {
+        if(num == 0) return;
+        if(num % 2 == 1) sb.append(idx).append(" ");
+        binary(num / 2, idx + 1);
     }
-
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int t = Integer.parseInt(br.readLine());
+        for(int i=0; i<t; i++) {
             int n = Integer.parseInt(br.readLine());
-            recursive(n);
-            for (int j = 0; j < sb.length(); j++) {
-                if(sb.charAt(j) == '1') {
-                    answer.append(j).append(" ");
-                }
-            }
-            sb.setLength(0);
-            answer.append("\n");
+            binary(n, 0);
+            sb.append("\n");
         }
-        System.out.println(answer.toString());
+
+        bw.append(sb.toString());
+        bw.close();
+        br.close();
     }
 }
