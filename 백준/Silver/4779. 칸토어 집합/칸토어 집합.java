b8f66fa765f560401cca967c012ea8778a139d1a
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-08-07 19:04:46 +0900

    [Silver III] Title: 칸토어 집합, Time: 204 ms, Memory: 21852 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -1,32 +1,28 @@
 import java.io.*;
-public class Main {
-    static StringBuilder sb = new StringBuilder();
 
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            String input = br.readLine();
-            if(input == null || input.isEmpty()) break;
-            int N = Integer.parseInt(input);
-            char[] arr = "-".repeat((int) Math.pow(3, N)).toCharArray();
-            dfs(arr, 0, arr.length);
-            sb.append(arr);
-            sb.append("\n");
-        }
-        System.out.print(sb.toString());
-    }
-
-    static void dfs(char[] arr, int start, int length) {
-        if (length == 1) {
-            return;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] arr = new String[13];
+        arr[0] = "-";
+        for(int i=1; i<13; i++) {
+            arr[i] = "";
+            arr[i] += arr[i-1];
+            arr[i] += " ".repeat(arr[i-1].length());
+            arr[i] += arr[i-1];
         }
 
-        int partLength = length / 3;
-        for (int i = start + partLength; i < start + (partLength * 2); i++) {
-            arr[i] = ' ';
+        StringBuilder sb = new StringBuilder();
+        while(true) {
+            String s = br.readLine();
+            if(s == null || s.isEmpty()) break;
+            int n = Integer.parseInt(s);
+            sb.append(arr[n]).append("\n");
         }
-
-        dfs(arr, start, partLength);
-        dfs(arr, start + (partLength * 2), partLength);
+        bw.append(sb.toString());
+        bw.close();
+        br.close();
     }
 }
