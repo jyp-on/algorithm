b66d766802e3014e0d1038a685c7a6306a7f9646
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-08-17 21:00:36 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 76 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -2,54 +2,53 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] op = new int[4];
     static int N;
-    static int[] nums;
+    static int[] arr;
+    static int[] op;
     static int max = Integer.MIN_VALUE;
     static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        nums = new int[N];
-        for(int i=0; i<N; i++) {
-            nums[i] = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        op = new int[4];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        for(int i=0; i<4; i++) {
-            op[i] = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; i++) {
+            op[i] = Integer.parseInt(st.nextToken());
         }
-        DFS(1, nums[0]);
+
+        DFS(1, arr[0]);
         System.out.println(max);
         System.out.println(min);
     }
 
     static void DFS(int depth, int sum) {
-        if(depth == N) {
+        if (depth == N) {
             max = Math.max(max, sum);
             min = Math.min(min, sum);
-        } else {
-            for(int i=0; i<4; i++) {
-                if(op[i]>0) {
-                    op[i]--;
-                    if(i==0) {
-                        DFS(depth+1, sum+nums[depth]);
-                    } else if(i==1) {
-                        DFS(depth+1, sum-nums[depth]);
-                    } else if(i==2) {
-                        DFS(depth+1, sum*nums[depth]);
-                    } else {
-                        if(sum<0) {
-                            DFS(depth+1, ((sum*-1)/nums[depth])*-1);
-                        } else if(sum>0) {
-                            DFS(depth+1, sum/nums[depth]);
-                        } else {
-                            DFS(depth+1, 0);
-                        }
-                    }
-                    op[i]++;
+            return;
+        }
+
+        for (int k = 0; k < 4; k++) {
+            if (op[k] > 0) {
+                op[k]--;
+                switch (k) {
+                    case 0: DFS(depth + 1, sum + arr[depth]);
+                        break;
+                    case 1: DFS(depth + 1, sum - arr[depth]);
+                        break;
+                    case 2: DFS(depth + 1, sum * arr[depth]);
+                        break;
+                    case 3: DFS(depth + 1, sum / arr[depth]);
+                        break;
                 }
+                op[k]++;
             }
         }
     }
