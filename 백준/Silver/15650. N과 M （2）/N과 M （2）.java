73767300bfab615e234b92ce05d5417da6ff3a24
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-08-19 18:54:54 +0900

    [Silver III] Title: N과 M (2), Time: 64 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,39 +1,37 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
+    static int[] arr;
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = i + 1;
-        }
-
-        combination(arr, new int[M], 0, 0, M);
-
+        int[] out = new int[M];
+        combination(out, 1, 0);
+        System.out.println(sb.toString());
     }
 
-    public static void combination(int[] arr, int[] out, int start, int depth, int r) {
-        if (depth == r) {
-            StringBuilder sb = new StringBuilder();
+    public static void combination(int[] out, int start, int depth) {
+        if (depth == M) {
             for (int a : out) {
                 sb.append(a).append(" ");
             }
-            System.out.println(sb);
+            sb.append("\n");
             return;
         }
-        for (int i = start; i < arr.length; i++) {
-            out[depth] = arr[i];
-            combination(arr, out, i + 1, depth + 1, r);
-
+        for (int i = start; i <= N; i++) {
+            out[depth] = i;
+            combination(out, i + 1, depth + 1);
         }
     }
 }
