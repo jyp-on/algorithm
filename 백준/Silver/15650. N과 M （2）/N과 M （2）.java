8ff76a940ae984597c97a42225bdfad24d70ee4f
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-08-19 09:50:48 +0900

    [Silver III] Title: N과 M (2), Time: 64 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,38 +1,39 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static boolean[] visited;
-    static void DFS(int depth, int start, String str) {
-        if(depth == M) {
-            System.out.println(str.replaceFirst(" ", ""));
-        } else {
-            for(int i = start; i <= N; i++) {
-                if(!visited[i]) {
-                    visited[i] = true;
-                    DFS(depth + 1, i, str+" "+i);
-                    visited[i] = false;
-                }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = i + 1;
+        }
+
+        combination(arr, new int[M], 0, 0, M);
+
+    }
+
+    public static void combination(int[] arr, int[] out, int start, int depth, int r) {
+        if (depth == r) {
+            StringBuilder sb = new StringBuilder();
+            for (int a : out) {
+                sb.append(a).append(" ");
             }
+            System.out.println(sb);
+            return;
+        }
+        for (int i = start; i < arr.length; i++) {
+            out[depth] = arr[i];
+            combination(arr, out, i + 1, depth + 1, r);
+
         }
     }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        visited = new boolean[N + 1];
-        DFS(0, 1, "");
-
-
-//        bw.write(answer.toString() + "\n");
-//        bw.flush();
-//        bw.close();
-//        br.close();
-    }
-
 }
