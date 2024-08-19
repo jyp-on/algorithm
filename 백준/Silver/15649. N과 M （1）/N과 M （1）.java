26b188a8c21eac804094fd4f76928400e243a03a
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-08-19 10:11:04 +0900

    [Silver III] Title: N과 M (1), Time: 192 ms, Memory: 24196 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,22 +2,21 @@ import java.util.*;
 public class Main {
     static int N, M;
     static boolean[] visited;
-    static int[] output;
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
         visited = new boolean[N + 1];
-        output = new int[M];
-        DFS(0);
+        int[] out = new int[M];
+        DFS(out, 0);
         System.out.println(sb.toString());
     }
 
-    static void DFS(int depth) {
+    static void DFS(int[] out, int depth) {
         if (depth == M) {
             for (int i = 0; i < M; i++) {
-                sb.append(output[i]).append(" ");
+                sb.append(out[i]).append(" ");
             }
             sb.append("\n");
             return;
@@ -26,8 +25,8 @@ public class Main {
         for (int i = 1; i <= N; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                output[depth] = i;
-                DFS(depth + 1);
+                out[depth] = i;
+                DFS(out, depth + 1);
                 visited[i] = false;
             }
         }
