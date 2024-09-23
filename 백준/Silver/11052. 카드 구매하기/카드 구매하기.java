76b6cb4ccba71ab64e14483bdba080fd435eab8d
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-09-23 21:23:36 +0900

    [Silver I] Title: 카드 구매하기, Time: 84 ms, Memory: 11896 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -1,33 +1,30 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main
-{
-    static int N;
-    static int P[] = new int[1001];
-    static int D[] = new int[1001];	// 수익
-
-    public static void main(String[] args) throws Exception
-    {
+public class Main {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-
-        String input = br.readLine();
-        StringTokenizer st = new StringTokenizer(input);
-        for (int i = 1; i <= N; i++)
-        {
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[] P = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
             P[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i <= N; i++)
-        {
-            for (int j = 1; j <= i; j++)
-            {
-                D[i] = Math.max(D[i], D[i-j] + P[j]);
+        int[] dp = new int[N+1]; // k개의 카드를 살때의 최대 값
+        // D[1] => P[1] = 1
+        // D[2] => (D[0] + P[2], D[1] + P[1]) = 5
+        // D[3] => (D[0] + P[3], D[1] + P[2], D[2] + P[1]) = 6
+
+        for(int i=1; i<=N; i++) {
+            for(int j=1; j<=i; j++) {
+                dp[i] = Math.max(dp[i], dp[i-j] + P[j]);
             }
         }
 
-        System.out.println(D[N]);
+        System.out.println(dp[N]);
     }
 }
