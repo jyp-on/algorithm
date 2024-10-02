47b61e7918029003a161c220bbd3ff69e6962e6d
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-10-02 17:31:38 +0900

    [Silver I] Title: 경로 찾기, Time: 124 ms, Memory: 15128 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,39 +1,36 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int n;
-    static int[][] graph;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken());
-        graph = new int[n][n];
-        for(int i=0; i<n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for(int j=0; j<n; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[][] matrix = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int k=0; k<n; k++) { // 중간 노드
-            for(int i=0; i<n; i++) {
-                for(int j=0; j<n; j++) {
-                    if(graph[i][k]==1 && graph[k][j]==1) {
-                        graph[i][j] = 1;
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (matrix[i][k] == 1 && matrix[k][j] == 1) {
+                        matrix[i][j] = 1;
                     }
                 }
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                sb.append(graph[i][j] + " ");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(matrix[i][j] + " ");
             }
             sb.append("\n");
         }
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
