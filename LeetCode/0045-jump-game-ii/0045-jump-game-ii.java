b9c0ce06e7999b022c8a0e1c7ce8f4752dfe718d
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2024-10-24 09:46:09 +0900

    Time: 41 ms (15.72%), Space: 45.2 MB (35.35%) - LeetHub

---
+++ LeetCode/0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int jump(int[] nums) {
+        int[] memo = new int[nums.length];
+        boolean flag = false;
+        for(int i=0; i<nums.length; i++) {
+            if(flag) break;
+            int p = nums[i];
+            for(int j=i+1; j<Math.min(i+p+1, nums.length); j++) {
+                if(memo[j] == 0) {
+                    memo[j] = memo[i] + 1;
+                } else {
+                    memo[j] = Math.min(memo[i] + 1, memo[j]);
+                }
+                
+                if(j == nums.length-1) {
+                    flag = true;
+                    break;
+                }
+            }
+        }
+
+        // for(int x : memo) System.out.print(x + " ");
+
+        return memo[nums.length-1];
+    }
+}
