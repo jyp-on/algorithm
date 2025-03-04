dc112650116a669345d8f50e0e6e5e18bfa344d0
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2025-03-04 17:13:29 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,6 +1,4 @@
-select distinct(d.ID), d.EMAIL, d.FIRST_NAME, d.LAST_NAME
-from DEVELOPERS d
-join SKILLCODES s on d.SKILL_CODE & s.CODE
-where s.NAME = 'C#'
-    or s.NAME = 'Python'
-order by ID;
+select distinct(d.id), d.email, d.first_name, d.last_name from developers d
+join skillcodes s on d.skill_code & s.code
+where s.name in ('Python', 'C#')
+order by id;
