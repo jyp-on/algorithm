a14e6bbb6bd546a03b52667dd211058a0bc2b8b7
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2025-07-06 18:03:56 +0900

    [Silver I] Title: 지름길, Time: 64 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.java
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -1,11 +1,7 @@
 import java.util.*;
 import java.io.*;
-import java.math.*;
-
 
 public class Main {
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,9 +10,9 @@ public class Main {
 
         int[][] arr = new int[n][3];
 
-        for(int i = 0;i<n;i++){
+        for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j = 0;j<3;j++){
+            for(int j=0; j<3; j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
@@ -27,20 +23,21 @@ public class Main {
             dp[i] = i;
         }
 
-        for(int i = 1; i <= m; i++) {
-            dp[i] = Math.min(dp[i], dp[i-1] + 1);
-            for(int j = 0; j < n; j++) {
-                if(arr[j][1] == i) { // 도착 지점이 i 일 경우 최소값 비교
+        for(int i=1; i<=m; i++) {
+            // 지름길을 타지 않는 경우가 더 빠를 수 있으니 직전 + 1 로 항상 업데이트
+            dp[i] = Math.min(dp[i], dp[i-1]+1);
+            for(int j=0; j<n; j++) { // 지름길 조회
+                if(arr[j][1]==i) { // 도착지가 i인 경우
+                    // Math.min(현재까지의 최단거리, 지름길 시작점 까지의 최단거리 + 해당 지름길을 탔을 경우 소모되는 거리) 중 더 적은 경우 선택
                     dp[i] = Math.min(dp[i], dp[arr[j][0]] + arr[j][2]);
                 }
             }
         }
 
-//        for(int i = 0;i<=m;i++){
+//        for(int i=0; i<=m; i++){
 //            System.out.printf("%d ", dp[i]);
 //        }
 
         System.out.println(dp[m]);
-
     }
 }
