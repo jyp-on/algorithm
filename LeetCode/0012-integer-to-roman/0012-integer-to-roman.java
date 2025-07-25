ddcb2088900baf6f9b1cc9672478c3c9083d17c9
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2025-07-25 17:15:21 +0900

    Time: 3 ms (98.18%), Space: 44.9 MB (16.97%) - LeetHub

--- LeetCode/0012-integer-to-roman/0012-integer-to-roman.java
+++ LeetCode/0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,14 +1,17 @@
 class Solution {
     public String intToRoman(int num) {
-        int A = num / 1000;
-        int B = num / 100;
-        int C = num / 10;
-        int D = num % 10;
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-        System.out.println(A);
-        System.out.println(B);
-        System.out.println(C);
-        System.out.println(D);
-        return null;
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                result.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        
+        return result.toString();
     }
 }
