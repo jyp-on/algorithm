32e1b190a7b722d2c8e87c907c8e119e53e94009
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2025-08-02 20:56:43 +0900

    Time: 6 ms (20.33%), Space: 45.3 MB (9.62%) - LeetHub

--- LeetCode/0013-roman-to-integer/0013-roman-to-integer.java
+++ LeetCode/0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,35 +1,22 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<String, Integer> map = new HashMap<>();
-        map.put("I", 1);
-        map.put("V", 5);
-        map.put("X", 10);
-        map.put("L", 50);
-        map.put("C", 100);
-        map.put("D", 500);
-        map.put("M", 1000);
+        Map<Character, Integer> map = Map.of(
+            'I', 1, 'V', 5, 'X', 10,
+            'L', 50, 'C', 100, 'D', 500, 'M', 1000
+        );
 
-        map.put("IV", 4);
-        map.put("IX", 9);
-        map.put("XL", 40);
-        map.put("XC", 90);
-        map.put("CD", 400);
-        map.put("CM", 900);
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int curr = map.get(s.charAt(i));
+            int next = (i + 1 < s.length()) ? map.get(s.charAt(i + 1)) : 0;
 
-        int ans = 0;
-
-        for(int i=0; i<s.length(); i++) {
-            String sub = s.substring(i, Math.min(s.length(), i+2));
-            
-            // System.out.println(sub);
-            if(map.containsKey(sub)) {
-                ans += map.get(sub);
-                i++;
+            if (curr < next) {
+                result -= curr;
             } else {
-                ans += map.get(s.substring(i, i+1));
+                result += curr;
             }
         }
 
-        return ans;
+        return result;
     }
 }
