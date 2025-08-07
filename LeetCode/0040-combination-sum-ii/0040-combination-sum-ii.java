7c38fb8e94562eb2b1951628fdbef710c5c9e8e5
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2025-08-07 20:17:01 +0900

    Time: 4 ms (36.67%), Space: 43.4 MB (52.75%) - LeetHub

---
+++ LeetCode/0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,61 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> answer = new ArrayList<>();
+        Arrays.sort(candidates);
+        // 1, 1, 2, 5, 6, 7, 10
+        bt(0, 0, candidates, target, new ArrayList<>(), answer);
+        return answer;
+    }
+
+    void bt(int start, int sum, int[] candidates, int target, List<Integer> path, List<List<Integer>> answer) {
+        // System.out.printf("start: %d, sum: %d, path: [", start, sum);
+        // for(int i : path) {
+        //     System.out.printf("%d, ", i);
+        // }
+        // System.out.println("]");
+
+        if(sum > target) return;
+        if(sum == target) {
+            answer.add(new ArrayList<>(path));
+            return;
+        }
+
+        for(int i=start; i<candidates.length; i++) {
+            if(i > start && candidates[i] == candidates[i-1]) continue;
+            path.add(candidates[i]);
+            bt(i+1, sum+candidates[i], candidates, target, path, answer);
+            path.remove(path.size()-1);
+        }
+    }
+
+    // start 0
+    // path.add(1);
+    // start, sum, path
+    // bt(1, 1, [1])
+
+    // start 1
+    // path.add(1);
+    // bt(2, 2, [1, 1])
+
+    // start 2
+    // path.add(2)
+    // bt(3, 4, [1, 1, 2])
+
+    // start 3
+    // path.add(5)
+    // bt(4, 9, [1, 1, 2, 5])
+
+    // start 3
+    // path.add(7)
+    // bt(4, 11, [1, 1, 2, 7])
+
+    // start 3
+    // path.add(7)
+    // bt(4, 14, [1, 1, 2, 10])
+
+    // start 3
+    // path.add(7)
+    // bt(4, 14, [1, 1, 2, 10])
+
+    // 
+}
