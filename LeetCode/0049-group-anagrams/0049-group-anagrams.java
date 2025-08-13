103d7155e50d0b713dad6b026f9e5fe2ae2b362e
JuYoung Park <52206904+jyp-on@users.noreply.github.com>
2025-08-13 21:11:26 +0900

    Time: 6 ms (98.62%), Space: 47.7 MB (91.5%) - LeetHub

--- LeetCode/0049-group-anagrams/0049-group-anagrams.java
+++ LeetCode/0049-group-anagrams/0049-group-anagrams.java
@@ -1,23 +1,13 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> answer = new ArrayList<>();
-        Map<Map<Character, Integer>, List<String>> map = new HashMap<>();
-        for(String str : strs) {
-            Map<Character, Integer> temp = new HashMap<>();
-            for(char c : str.toCharArray()) {
-                temp.put(c, temp.getOrDefault(c, 0)+1);
-            }
-            map.putIfAbsent(temp, new ArrayList<>());
-            map.get(temp).add(str);
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);   // 정렬된 문자열을 key로 사용
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
         }
-
-        for(Map.Entry<Map<Character, Integer>, List<String>> m : map.entrySet()) {
-            answer.add(m.getValue());
-            // System.out.println(m.getKey().toString());
-            // System.out.println(m.getValue().toString());
-            // System.out.println("======================");
-        }
-
-        return answer;
+        return new ArrayList<>(map.values());
     }
 }
